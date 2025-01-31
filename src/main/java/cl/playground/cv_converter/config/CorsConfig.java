c1e85c8ba060d9c89347c673363c15da04345b24
Andres Sepulveda <andressep.95@gmail.com>
2025-01-31 02:39:57 -0300

    first try cors

--- src/main/java/cl/playground/cv_converter/config/CorsConfig.java
+++ src/main/java/cl/playground/cv_converter/config/CorsConfig.java
@@ -1,28 +1,34 @@
 package cl.playground.cv_converter.config;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
 public class CorsConfig {
 
-    @Value("${cors.allowed-origins}") // Inyecta el valor desde application.properties
-    private String[] allowedOrigins;
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
+                registry.addMapping("/api/v1/resume")
                     .allowedOrigins(allowedOrigins)
-                    .allowedMethods("POST")
-                    .allowedHeaders("*");
+                    .allowedMethods("POST", "OPTIONS")
+                    .allowedHeaders("Content-Type");
             }
         };
     }
 
+    @PostConstruct
+    public void init() {
+        System.out.println("Allowed Origins: " + allowedOrigins);
+    }
 }
