29838b4d813797f5d47c643b032d04f3b6bdabda
Andres Sepulveda <andressep.95@gmail.com>
2025-01-31 02:54:07 -0300

    second try cors

--- src/main/java/cl/playground/cv_converter/config/CorsConfig.java
+++ src/main/java/cl/playground/cv_converter/config/CorsConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 @Configuration
 public class CorsConfig {
 
@@ -22,7 +21,7 @@ public class CorsConfig {
                 registry.addMapping("/api/v1/resume")
                     .allowedOrigins(allowedOrigins)
                     .allowedMethods("POST", "OPTIONS")
-                    .allowedHeaders("Content-Type");
+                    .allowedHeaders("*");
             }
         };
     }
